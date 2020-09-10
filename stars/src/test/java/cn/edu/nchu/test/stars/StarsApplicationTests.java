6108cdacde2c27728812d8008904d6a85adbe617
fire-g <51811894+fire-g@users.noreply.github.com>
2020-09-10 16:32:42 +0800

    重建项目

---
+++ stars/src/test/java/cn/edu/nchu/test/stars/StarsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nchu.test.stars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
