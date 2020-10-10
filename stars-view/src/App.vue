c07842c1aa34dde18e19a420b7a8110f89ae8164
fire-g <18296517259@163.com>
2020-10-10 16:00:19 +0800

    add bootstrap-vue and modify some component

--- stars-view/src/App.vue
+++ stars-view/src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <Nav/>
+    <Navigation/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav'
 export default {
   name: 'App',
-  components: {Nav}
 }
 </script>
 
