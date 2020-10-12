132725902c24ccad7d92006131ae76a0ed35ad38
LUZaLID-lzl <LUZaLID@outlook.com>
2020-10-12 09:56:52 +0800

    1

--- stars-view/src/components/login.vue
+++ stars-view/src/components/login.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div id="app">
     <div class="logo"></div>
     <div class="form-item" id='form-div1'>
-      <input id="emil" type="text" autocomplete="off" placeholder="邮箱">
+      <input id="emil" type="text" autocomplete="off" placeholder="邮箱" :value="emil">
     </div>
     <div class="form-item" id='form-div2'>
-      <input id="password" type="password" autocomplete="off" placeholder="登录密码">
-      <div class='forgot'><a href="register.vue"
+      <input id="password" type="password" autocomplete="off" placeholder="登录密码" :value="password">
+      <div class='forgot'><a href="register"
                              style='color: #1cccf4;font-size: 16px;text-decoration: none;'>忘记密码?</a></div>
-      <p class="tip" id='loginPrompt'>邮箱或密码不正确</p>
+      <p class="tip">{{this.loginPrompt}}</p>
     </div>
     <!-- <div class="form-item">
         <select id = 'getId'>
@@ -19,17 +19,60 @@
     </div>
     -->
     <div class="form-item">
-      <button id="submit">登 录</button>
+      <button id="submit" @click="login()">登 录</button>
     </div>
     <div class="reg-bar">
-      <p style='color: #ffffff; font-family: Tahoma,Arial,"宋体",serif;'>还没有账号？点击这里 <a href="register.vue">进行注册</a></p>
+      <p style='color: #ffffff; font-family: Tahoma,Arial,"宋体",serif;'>还没有账号？点击这里 <a href="register">进行注册</a></p>
     </div>
   </div>
 </template>
 
 <script>
+import $ajax from 'axios'
+export default {
+  data () {
+    return {
+      emil: '',
+      password: '',
+      loginPrompt: '',
+      isSuccess: false
+    }
+  },
+  created () {
+  },
+  methods: {
+    login () {
+      var emil = document.getElementById('emil').value
+      var password = document.getElementById('password').value
+      var form = new FormData()
+      form.append('email', emil)
+      form.append('password', password)
+      $ajax({
+        method: 'POST',
+        url: '/api/login/',
+        data: form
+      }).then((response) => {
+        const res = response.data
+        // eslint-disable-next-line eqeqeq
+        if (res.code == 400) {
+          this.isSuccess = false
+          this.loginPrompt = '邮箱或密码错误'
+          // eslint-disable-next-line eqeqeq
+        } else if (res.code == 0) {
+          this.$router.push('/')
+          this.isSuccess = true
+        } else {
+          this.isSuccess = false
+          this.loginPrompt = '请输入密码'
+        }
+        console.log(res.loginPrompt)
+        console.log(res.message)
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
-@import "../css/login.css";
+/*@import "../css/login.css";*/
 </style>
--- stars-view/src/components/register.vue
+++ stars-view/src/components/register.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>
+  <div id="app">
+    <div style="height:100%">
+      <div class="logo"></div>
+      <div class="form-item">
+        <input id="emil" type="text" autocomplete="off" placeholder="邮箱" onchange="checkEmail()">
+        <p class="tip" id='registerEmail'>邮箱格式不正确</p>
+      </div>
+      <div class="form-item">
+        <input id="varife" type="text" autocomplete="off" placeholder="验证码">
+        <input type = 'button' id="getCode" onclick = 'nowTime(this)' value = '获取验证码'>
+      </div>
+      <div class="form-item">
+        <input id="password" type="password" autocomplete="off" placeholder="登录密码" onchange="checkPassword()">
+      </div>
+      <div class="form-item">
+        <input id="passwordRepeat" type="password" autocomplete="off" placeholder="确认密码" onchange="checkPassword()">
+        <p class="tip" id='registerPrompt'>密码输入不一致</p>
+      </div>
+
+      <div class="form-item">
+        <input id="sno" type="text" autocomplete="off" placeholder="学号">
+      </div>
+      <div class="form-item">
+        <input id="name" type="text" autocomplete="off" placeholder="姓名">
+      </div>
+      <div class="form-item">
+        <button id="submit" onclick = "submitMessage()">注  册</button>
+      </div>
+
+      <div class="form-item">
+        <button id="next" onclick = "change()">下一步</button>
+      </div>
+      <div class="reg-bar">
+        <p style='color: #ffffff; font-family: Tahoma,Arial,"宋体";'>已有账号？点击这里 <a href="login">登录</a></p>
+
+      </div>
+    </div>
+    <div style="text-align:center;"></div>
   </div>
 </template>
 
 <script>
+import $ajax from 'axios'
+export default {
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
 </script>
 
 <style scoped>
+@import "../css/register.css";
 
 </style>
