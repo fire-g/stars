35f581968aeb0918631d22b8f7300e9121960c74
fire-g <18296517259@163.com>
2020-10-12 10:40:50 +0800

    修复测试账号

--- weatherforecast/src/main/resources/db/initDB.sql
+++ weatherforecast/src/main/resources/db/initDB.sql
@@ -106,7 +106,7 @@ CREATE TABLE user (
     PRIMARY KEY (id)
 );
 
-//邮箱:18296517259@163.com
-//密码:123456
+-- 邮箱:18296517259@163.com
+-- 密码:123456
 INSERT INTO user (id, username, email, password, salt)
-values (1,'测试','18296517259@163.com','b06cff8cdc8df9f59722c784b6a215b0','4ef80102-50a6-4908-9969-109bab2d1546')
+values (1,'测试','18296517259@163.com','b06cff8cdc8df9f59722c784b6a215b0','4ef80102-50a6-4908-9969-109bab2d1546');
